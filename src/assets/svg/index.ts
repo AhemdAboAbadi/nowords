dc42f267ff1f305a362cd5aadbff8b935773f859
momenfnnana <moamenfnnana1@gmail.com>
2025-05-16 18:19:48 +0300

    Add Eye and User SVG icons; update index.ts to export new icons

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,3 +2,5 @@ export {default as EmailIcon} from './emailIcon.svg';
 export {default as LockPasswordIcon} from './lockPassword.svg';
 export {default as ArrowsSvg} from './arrows.svg';
 export {default as PhoneIcon} from './phoneIcon.svg';
+export {default as EyeIcon} from './eye.svg';
+export {default as userIcon} from './user.svg';
