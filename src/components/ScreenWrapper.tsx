940fdaef633b21939ccca28821141b536bbcd605
momenfnnana <moamenfnnana1@gmail.com>
2025-05-16 18:20:15 +0300

    Enhance ScreenWrapper component to support optional scrollable prop, allowing content to be wrapped in a ScrollView for improved usability on smaller screens.

--- src/components/ScreenWrapper.tsx
+++ src/components/ScreenWrapper.tsx
@@ -1,9 +1,10 @@
-import {Image, Text, View} from 'react-native';
+import {Image, Text, View, ScrollView} from 'react-native';
 import React, {FC, ReactNode} from 'react';
 
 interface ScreenWrapperProps {
   children: ReactNode | ReactNode[];
   title?: string;
+  scrollable?: boolean;
 }
 
 const image = require('../assets/images/backgroundImg.png');
@@ -11,9 +12,12 @@ const image = require('../assets/images/backgroundImg.png');
 const ScreenWrapper: FC<ScreenWrapperProps> = ({
   children,
   title = 'Welcome Back!',
+  scrollable = false,
 }) => {
+  const Wrapper = scrollable ? ScrollView : View;
+
   return (
-    <View className="flex-1 bg-gray relative">
+    <Wrapper className="flex-1 bg-gray relative">
       <Image
         source={image}
         className="absolute top-0 left-0 w-full h-[302px] z-0 bg-[#3E8283] rounded-b-[30px]"
@@ -22,7 +26,7 @@ const ScreenWrapper: FC<ScreenWrapperProps> = ({
         {title}
       </Text>
       <View className="flex-1 bottom-20 justify-center z-10">{children}</View>
-    </View>
+    </Wrapper>
   );
 };
 
