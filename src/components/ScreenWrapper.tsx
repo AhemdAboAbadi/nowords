906e79cb9c6db14c3e61a75a7721d817ebd31d1c
momenfnnana <moamenfnnana1@gmail.com>
2025-05-16 14:06:17 +0300

    Enhance ScreenWrapper component to accept a customizable title prop, defaulting to 'Welcome Back!'. This allows for greater flexibility in displaying different titles within the screen wrapper.

--- src/components/ScreenWrapper.tsx
+++ src/components/ScreenWrapper.tsx
@@ -3,11 +3,15 @@ import React, {FC, ReactNode} from 'react';
 
 interface ScreenWrapperProps {
   children: ReactNode | ReactNode[];
+  title?: string;
 }
 
 const image = require('../assets/images/backgroundImg.png');
 
-const ScreenWrapper: FC<ScreenWrapperProps> = ({children}) => {
+const ScreenWrapper: FC<ScreenWrapperProps> = ({
+  children,
+  title = 'Welcome Back!',
+}) => {
   return (
     <View className="flex-1 bg-gray relative">
       <Image
@@ -15,7 +19,7 @@ const ScreenWrapper: FC<ScreenWrapperProps> = ({children}) => {
         className="absolute top-0 left-0 w-full h-[302px] z-0 bg-[#3E8283] rounded-b-[30px]"
       />
       <Text className="text-white text-5xl font-bold self-center mt-20">
-        Welcome Back!
+        {title}
       </Text>
       <View className="flex-1 bottom-20 justify-center z-10">{children}</View>
     </View>
